d4729216de08193e5aaf73c6689f795e0b8d1768
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2025-05-19 10:46:35 +0300

    added nvim notify and hardtime

---
+++ lua/witcher/plugins/notify.lua
@@ -0,0 +1,29 @@
+return {
+  'rcarriga/nvim-notify',
+  lazy = false,
+  config = function()
+    local notify = require('notify')
+    vim.notify = notify
+    notify.setup({
+      background_colour = '#1f1f1f',
+      fps = 30,
+      icons = {
+        DEBUG = '',
+        ERROR = '󰅚',
+        INFO = '󰋽',
+        TRACE = '',
+        WARN = '󰀪',
+      },
+      level = 2,
+      minimum_width = 50,
+      render = 'default',
+      stages = 'fade_in_slide_out',
+      time_formats = {
+        notification = '%T',
+        notification_history = '%FT%T',
+      },
+      timeout = 5000,
+      top_down = true,
+    })
+  end,
+}
