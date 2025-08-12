7684f4e3b0c402c551812bc4d2c858ac67164270
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2025-08-12 17:55:07 +0300

    more telescope settings

--- lua/witcher/plugins/telescope.lua
+++ lua/witcher/plugins/telescope.lua
@@ -8,17 +8,32 @@ return {
   },
   config = function()
     -- set keymaps
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
+    local actions = require('telescope.actions')
+    local undo_actions = require('telescope-undo.actions')
     local keymap = vim.keymap
 
-    keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Fuzzy find files in cwd' })
-    keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Fuzzy find recent files' })
-    keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
-    keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<cr>', { desc = 'Find git commits' })
-    keymap.set('n', '<leader>gb', '<cmd>Telescope git_bcommits<cr>', { desc = 'Find git commits for buffer' })
-    keymap.set('n', '<leader>fk', '<cmd>Telescope keymaps<cr>', { desc = 'Find keymaps' })
-    keymap.set('n', '<leader>fw', '<cmd>Telescope grep_string<cr>', { desc = 'Find word under cursor' })
-    keymap.set('n', '<leader>fu', '<cmd>Telescope undo<cr>', { desc = 'Find in undo tree' })
-    require('telescope').setup({
+    keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Fuzzy find files in cwd' })
+    keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Fuzzy find recent files' })
+    keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+    keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Find git commits' })
+    keymap.set('n', '<leader>gb', builtin.git_bcommits, { desc = 'Find git commits for buffer' })
+    keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find keymaps' })
+    keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Find word under cursor' })
+    keymap.set('n', '<leader>fr', builtin.registers, { desc = 'Find registers' })
+    keymap.set('n', '<leader>fu', telescope.extensions.undo.undo, { desc = 'Find in undo tree' })
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-y>'] = actions.select_default,
+          },
+          n = {
+            ['<C-y>'] = actions.select_default,
+          },
+        },
+      },
       extensions = {
         undo = {
           use_delta = true,
@@ -28,22 +43,22 @@ return {
           entry_format = 'state #$ID, $STAT, $TIME',
           mappings = {
             i = {
-              ['<cr>'] = require('telescope-undo.actions').yank_additions,
-              ['<S-cr>'] = require('telescope-undo.actions').yank_deletions,
-              ['<C-cr>'] = require('telescope-undo.actions').restore,
+              ['<cr>'] = undo_actions.yank_additions,
+              ['<S-cr>'] = undo_actions.yank_deletions,
+              ['<C-cr>'] = undo_actions.restore,
               -- alternative defaults, for users whose terminals do questionable things with modified <cr>
-              ['<C-y>'] = require('telescope-undo.actions').yank_deletions,
-              ['<C-r>'] = require('telescope-undo.actions').restore,
+              ['<C-y>'] = undo_actions.yank_deletions,
+              ['<C-r>'] = undo_actions.restore,
             },
             n = {
-              ['y'] = require('telescope-undo.actions').yank_additions,
-              ['Y'] = require('telescope-undo.actions').yank_deletions,
-              ['u'] = require('telescope-undo.actions').restore,
+              ['y'] = undo_actions.yank_additions,
+              ['Y'] = undo_actions.yank_deletions,
+              ['u'] = undo_actions.restore,
             },
           },
         },
       },
     })
-    require('telescope').load_extension('undo')
+    telescope.load_extension('undo')
   end,
 }
