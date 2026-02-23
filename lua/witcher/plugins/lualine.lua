f327dc8d3e611559b1bf2c555d4dbaee14f84096
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2026-02-23 08:57:34 +0300

    changed lualine cs

--- lua/witcher/plugins/lualine.lua
+++ lua/witcher/plugins/lualine.lua
@@ -2,6 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup({ options = { theme = 'onedark' } })
+    require('lualine').setup({ options = { theme = 'vscode-theme-for-nvim' } })
   end,
 }
